c4bc35d69aeb4105ae2d74e6c9e9a8339fbb1f9b
Jader Martins <jadermcs94@gmail.com>
2022-05-08 21:23:15 -0300

    update

--- train/ta_noencode.py
+++ train/ta_noencode.py
@@ -48,7 +48,10 @@ def main(raw_args=None):
     column_names = datasets["train"].column_names
 
     def tokenizer_function(examples):
-        return tokenizer(examples["text"])
+        res = tokenizer(examples["text"], truncation=True, padding="max_length",
+                        max_length=args.token_length)
+        res['labels'] = res['input_ids'].copy()
+        return res
 
     column_names = datasets["train"].column_names
 
