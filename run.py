3b0e289de8c8a7efadea4ad3ec5ca3ab03d8ed5b
Jader Martins <jadermcs94@gmail.com>
2022-07-05 17:47:21 -0300

    update

--- run.py
+++ run.py
@@ -17,10 +17,10 @@ GPT_LIST = [
 ]
 
 if __name__ == "__main__":
-    # print("Processing tripadvisor...")
-    # tripadvisor.main()
-    # print("Processing tripadvisor (no encode)...")
-    # tripadvisor_noencode.main()
+    print("Processing tripadvisor...")
+    tripadvisor.main()
+    print("Processing tripadvisor (no encode)...")
+    tripadvisor_noencode.main()
     # print("Processing multiwoz...")
     # multiwoz.main()
 
