b76554f7f349c37503dd063a6630ae3866097c28
Jader Martins <jadermcs94@gmail.com>
2022-08-29 15:53:13 -0300

    update early stop

--- train/mwoz.py
+++ train/mwoz.py
@@ -87,6 +87,7 @@ def main(raw_args=None):
         f"{args.directory}",
         run_name=f"{args.directory}",
         evaluation_strategy="epoch",
+        save_strategy="epoch",
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
